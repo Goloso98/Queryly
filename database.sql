a8aa6965f9ef34e3c853acced87bc4f6721a47e9
up201908209 <up201908209@fc.up.pt>
2022-10-24 17:00:54 +0100

    creation of database added triggers and one index

--- database.sql
+++ database.sql
@@ -1,3 +1,7 @@
+DROP SCHEMA IF EXISTS lbaw2294 CASCADE;
+CREATE SCHEMA lbaw2294 AUTHORIZATION postgres;
+SET search_path TO lbaw2294;
+
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS roles CASCADE;
 DROP TABLE IF EXISTS posts CASCADE;
@@ -16,8 +20,13 @@ DROP TABLE if EXISTS new_comments CASCADE;
 DROP TABLE if EXISTS new_badges CASCADE;
 DROP TABLE if EXISTS new_stars CASCADE;
 
+DROP TYPE if EXISTS post_type CASCADE;
+DROP TYPE if EXISTS user_role CASCADE;
+DROP TYPE if EXISTS report_type CASCADE;
+
 CREATE TYPE post_type AS ENUM ('question', 'answer');
 CREATE TYPE user_role AS ENUM ('Moderator', 'Administrator');
+CREATE TYPE report_type AS ENUM ('post', 'comment');
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
@@ -36,20 +45,16 @@ CREATE TABLE roles (
 
 CREATE TABLE posts (
     id SERIAL PRIMARY KEY,
-    author VARCHAR NOT NULL REFERENCES "users" (id) ON UPDATE CASCADE,
+    userID INTEGER NOT NULL REFERENCES "users" (id) ON UPDATE CASCADE,
     date DATE NOT NULL,
     type post_type NOT NULL,
     title VARCHAR NOT NULL CHECK (type = 'question'),
     text VARCHAR NOT NULL,
-    isCorrect BOOLEAN NOT NULL CHECK (type = 'answer')
-);
-
-CREATE TABLE reports (
-
+    parentPost INTEGER REFERENCES "posts" (id) ON UPDATE CASCADE,
+    isCorrect BOOLEAN CHECK (type = 'answer')
 );
 
 CREATE TABLE stars (
-    id SERIAL PRIMARY KEY,
     postID INTEGER NOT NULL REFERENCES "posts" (id) ON UPDATE CASCADE,
     userID INTEGER NOT NULL REFERENCES "users" (id) ON UPDATE CASCADE
 );
@@ -57,9 +62,17 @@ CREATE TABLE stars (
 CREATE TABLE comments (
     id SERIAL PRIMARY KEY,
     postID INTEGER NOT NULL REFERENCES "posts" (id) ON UPDATE CASCADE,
-    authorID INTEGER NOT NULL REFERENCES "users" (id) ON UPDATE CASCADE,
+    userID INTEGER NOT NULL REFERENCES "users" (id) ON UPDATE CASCADE,
     text VARCHAR NOT NULL,
     date DATE NOT NULL
+);
+
+CREATE TABLE reports (
+    id SERIAL PRIMARY KEY,
+    type report_type NOT NULL,
+    userID INTEGER REFERENCES "users" (id) ON UPDATE CASCADE,
+    postID INTEGER REFERENCES "posts" (id) ON UPDATE CASCADE,
+    commentID INTEGER REFERENCES "comments" (id) ON UPDATE CASCADE
 );
 
 CREATE TABLE tags (
@@ -73,7 +86,7 @@ CREATE TABLE user_tags (
 );
 
 CREATE TABLE question_tags (
-    postID INTEGER NOT NULL REFERENCES "post" (id) ON UPDATE CASCADE,
+    postID INTEGER NOT NULL REFERENCES "posts" (id) ON UPDATE CASCADE,
     tagID INTEGER NOT NULL REFERENCES "tags" (id) ON UPDATE CASCADE
 );
 
@@ -89,38 +102,174 @@ CREATE TABLE user_badges (
 
 CREATE TABLE notifications (
     id SERIAL PRIMARY KEY,
-    postID INTEGER NOT NULL REFERENCES "post" (id) ON UPDATE CASCADE,
     userID INTEGER NOT NULL REFERENCES "users" (id) ON UPDATE CASCADE,
     isRead BOOLEAN NOT NULL,
-    date DATE NOT NULL
+    date DATE NOT NULL,
+    text VARCHAR NOT NULL
 );
 
 CREATE TABLE new_answers (
     notificationID INTEGER NOT NULL REFERENCES "notifications" (id) ON UPDATE CASCADE,
-    postID INTEGER NOT NULL REFERENCES "post" (id) ON UPDATE CASCADE,
-    text VARCHAR NOT NULL
+    postID INTEGER NOT NULL REFERENCES "posts" (id) ON UPDATE CASCADE,
+    questionID INTEGER NOT NULL REFERENCES "posts" (id) ON UPDATE CASCADE
 );
 
 CREATE TABLE new_questions (
     notificationID INTEGER NOT NULL REFERENCES "notifications" (id) ON UPDATE CASCADE,
-    postID INTEGER NOT NULL REFERENCES "post" (id) ON UPDATE CASCADE,
-    text VARCHAR NOT NULL
+    postID INTEGER NOT NULL REFERENCES "posts" (id) ON UPDATE CASCADE,
+    tagID INTEGER NOT NULL REFERENCES "tags" (id) ON UPDATE CASCADE
 );
 
 CREATE TABLE new_comments (
     notificationID INTEGER NOT NULL REFERENCES "notifications" (id) ON UPDATE CASCADE,
-    commentID INTEGER NOT NULL REFERENCES "comment" (id) ON UPDATE CASCADE,
-    text VARCHAR NOT NULL
+    commentID INTEGER NOT NULL REFERENCES "comments" (id) ON UPDATE CASCADE
 );
 
 CREATE TABLE new_badges (
     notificationID INTEGER NOT NULL REFERENCES "notifications" (id) ON UPDATE CASCADE,
-    badgeID INTEGER NOT NULL REFERENCES "badges" (id) ON UPDATE CASCADE,
-    text VARCHAR NOT NULL
+    badgeID INTEGER NOT NULL REFERENCES "badges" (id) ON UPDATE CASCADE
 );
 
 CREATE TABLE new_stars (
     notificationID INTEGER NOT NULL REFERENCES "notifications" (id) ON UPDATE CASCADE,
-    starID INTEGER NOT NULL REFERENCES "stars" (id) ON UPDATE CASCADE,
-    text VARCHAR NOT NULL
+    postID INTEGER NOT NULL REFERENCES "posts" (id) ON UPDATE CASCADE
 );
+
+-- TRIGGERS
+
+DROP FUNCTION IF EXISTS add_answer_notification CASCADE;
+DROP FUNCTION IF EXISTS add_question_notification() CASCADE;
+DROP FUNCTION IF EXISTS add_comment_notification() CASCADE;
+DROP FUNCTION IF EXISTS add_star_notification() CASCADE;
+
+--answer notifications
+CREATE FUNCTION add_answer_notification() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+    IF NEW.type = 'answer' THEN
+        WITH parent_post AS (
+            SELECT parentPost FROM posts WHERE posts.id = NEW.id
+        ),
+        notified_user AS (
+            SELECT author FROM posts WHERE posts.id = parent_post.id
+        ),
+        inserted AS (
+            INSERT INTO notifications (userID, isRead, date, text)
+            VALUES (notified_user.id, FALSE, CURRENT_TIMESTAMP, concat('You have a new answer on question ', (SELECT title FROM posts where NEW.parentPost = id), '!'))
+            RETURNING id
+        )
+        INSERT INTO new_answers SELECT inserted.id, NEW.id, NEW.parentPost FROM inserted;
+    END IF;
+    RETURN NEW;
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER answer_trigger
+    BEFORE INSERT OR UPDATE ON posts
+    FOR EACH ROW
+    EXECUTE PROCEDURE add_answer_notification();
+
+--question tag notifications
+CREATE OR REPLACE FUNCTION add_question_notification() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+    WITH notified_users AS (
+        SELECT userID FROM user_tags
+        WHERE NEW.tagID = user_tags.tagID
+    ),
+    inserted AS (
+        INSERT INTO notifications (userID, isRead, date, text)
+        VALUES (notified_users.id, FALSE, CURRENT_TIMESTAMP, concat('There is a new question on a tag you follow: ', (SELECT title FROM posts where NEW.id = id), '!'))
+        RETURNING id
+    )
+    INSERT INTO new_questions SELECT inserted.id, NEW.id, NEW.tagID FROM inserted;
+    RETURN NEW;
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER add_question_notification
+    BEFORE INSERT OR UPDATE ON question_tags
+    FOR EACH ROW
+    EXECUTE PROCEDURE add_question_notification();
+
+--comment notifications
+CREATE OR REPLACE FUNCTION add_comment_notification() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+    WITH inserted AS (
+        INSERT INTO notifications (userID, isRead, date, text)
+        VALUES (NEW.userID, FALSE, CURRENT_TIMESTAMP, concat('There is a new comment on one of your posts: ', (SELECT title FROM posts where NEW.postID = id), '!'))
+        RETURNING id
+    )
+    INSERT INTO new_comments SELECT inserted.id, NEW.badgeID FROM inserted;
+    RETURN NEW;
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER add_comment_notification
+    BEFORE INSERT OR UPDATE ON comments
+    FOR EACH ROW
+    EXECUTE PROCEDURE add_comment_notification();
+
+--star notifications
+CREATE OR REPLACE FUNCTION add_star_notification() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+    WITH notified_user AS (
+        SELECT author FROM posts
+        WHERE NEW.postID = posts.id
+    ),
+    inserted AS (
+        INSERT INTO notifications (userID, isRead, date, text)
+        VALUES (notified_user.id, FALSE, CURRENT_TIMESTAMP, concat('Congratulations!', (SELECT name FROM users WHERE NEW.userID = id) ,' liked your post: ', (SELECT title FROM posts where NEW.postID = id), '!'))
+        RETURNING id
+    )
+    INSERT INTO new_stars SELECT inserted.id, NEW.postID FROM inserted;
+    RETURN NEW;
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER add_star_notification
+    BEFORE INSERT OR UPDATE ON stars
+    FOR EACH ROW
+    EXECUTE PROCEDURE add_star_notification();
+
+-- FTS INDEXES
+
+DROP INDEX IF EXISTS search_user;
+-- DROP INDEX IF EXISTS search_question;
+-- DROP INDEX IF EXISTS search_answer;
+-- DROP INDEX IF EXISTS search_comment;
+
+ALTER TABLE users
+ADD COLUMN tsvectors TSVECTOR;
+
+CREATE FUNCTION user_search_update() RETURNS TRIGGER AS $$
+BEGIN
+    IF TG_OP = 'INSERT' THEN
+        NEW.tsvectors = ((setweight(to_tsvector('english', NEW.name), 'A')) || (setweight(to_tsvector('english', NEW.username), 'B')));
+    END IF;
+    IF TG_OP = 'UPDATE' THEN
+        IF ((NEW.name <> OLD.name) || (NEW.username <> OLD.username)) THEN
+            NEW.tsvectors = ((setweight(to_tsvector('english', NEW.name), 'A')) || (setweight(to_tsvector('english', NEW.username), 'B')));
+        END IF;
+    END IF;
+    RETURN NEW;
+END $$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER user_search_update
+    BEFORE INSERT OR UPDATE ON users
+    FOR EACH ROW
+    EXECUTE PROCEDURE user_search_update();
+
+CREATE INDEX search_user ON users USING GIN (tsvectors);
+
+
+-- INSERT INTO users VALUES (1, 'Margarida', 'mnps@example.com', 'mnps', 'lalala', TO_DATE('24/10/2001', 'DD/MM/YYYY'), FALSE);
+-- INSERT INTO posts VALUES (1, 1, TO_DATE('24/10/2022', 'DD/MM/YYYY'), 'question', 'birthday', 'is it my birthday?');
+-- INSERT INTO posts VALUES (2, 1, TO_DATE('24/10/2022', 'DD/MM/YYYY'), 'answer', 'yeps', 1, FALSE);
