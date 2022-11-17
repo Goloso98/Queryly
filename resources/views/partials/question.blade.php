24ce9a6e8bae97c7f8eefeb2e24a8b7a37e7065e
up201906031 <up201906031@fc.up.pt>
2022-11-17 17:29:03 +0000

    Added button to redirect to corresponding post

--- resources/views/partials/question.blade.php
+++ resources/views/partials/question.blade.php
@@ -1,6 +1,7 @@
 <article class="post" data-id="{{ $question->id }}">
 <header>
-    <h3>Title: {{ $question->title }} </h3>
+    <h3>Title: {{ $question->title }}</h3>
+    <a href="/posts/{{ $question->id }}">See Post</a>
     <a href="#" class="delete">Delete</a>
 </header>
 <p> {{ $question->posttext }} </p>
