7b4c725387d990bf5db75e2f6a474e67559c3a4f
Gabriel <gooloso98@gmail.com>
2022-11-18 01:33:58 +0000

    add breadcrumb

---
+++ resources/views/components/breadcrumb.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <ol class="breadcrumb">
+    @foreach (array_slice($url, 0, -1) as $elem)
+            <li class="breadcrumb-item"><a href="#">{{$elem}}</a></li>
+    @endforeach
+    </ol>
+</div>
+
+
+{{--<ol class="breadcrumb">--}}
+{{--    <li class="breadcrumb-item"><a href="#">Home</a></li>--}}
+{{--    <li class="breadcrumb-item active" aria-current="page">Library</li>--}}
+{{--</ol>--}}
