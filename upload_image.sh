c5c69db43b19f4dc6cb003fcf4ebd8547717f3d0
Gabriel <gooloso98@gmail.com>
2022-11-22 15:32:33 +0000

    https template merge

--- upload_image.sh
+++ upload_image.sh
@@ -3,13 +3,14 @@
 # Stop execution if a step fails
 set -e
 
-IMAGE_NAME=git.fe.up.pt:5050/lbaw/lbaw2122/lbaw2294 # Replace with your group's image name
+IMAGE_NAME=git.fe.up.pt:5050/lbaw/lbaw2223/lbaw2294 # Replace with your group's image name
 
 # Ensure that dependencies are available
 composer install
+php artisan config:clear
 php artisan clear-compiled
 php artisan optimize
 
-#docker buildx build --push --platform linux/amd64 -t $IMAGE_NAME .
+# docker buildx build --push --platform linux/amd64 -t $IMAGE_NAME .
 docker build -f Dockerfile.prod -t $IMAGE_NAME .
 docker push $IMAGE_NAME
