134bcb2d803241155294138944bf17705ce654d3
up201906031 <up201906031@fc.up.pt>
2022-11-24 19:33:51 +0000

    Updated Login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,30 +3,46 @@
 @section('content')
 <form method="POST" action="{{ route('login') }}">
     {{ csrf_field() }}
+    <p></p>
+    <h2 class="text-center">Welcome Back!</h2>
+    <p></p>
+    <hr>
+    <div class="input-group mb-3">
+      <span class="input-group-text">Email</span>
+      <div class="form-floating">
+        <input id="email" type="text" name="email" class="form-control" placeholder="Email">
+        @if ($errors->has('email'))
+          <span class="error">
+            {{ $errors->first('email') }}
+          </span>
+        @endif
+      </div>
+    </div>
 
-    <label for="email">E-mail</label>
-    <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
-    @if ($errors->has('email'))
-        <span class="error">
-          {{ $errors->first('email') }}
-        </span>
-    @endif
-
-    <label for="password" >Password</label>
-    <input id="password" type="password" name="password" required>
-    @if ($errors->has('password'))
-        <span class="error">
+    <div class="input-group mb-3">
+      <span class="input-group-text">Password</span>
+      <div class="form-floating">
+        <input id="password" type="password" name="password" class="form-control" placeholder="Password" required>
+        @if ($errors->has('password'))
+          <span class="error">
             {{ $errors->first('password') }}
-        </span>
-    @endif
+          </span>
+        @endif
+      </div>
+    </div>
 
-    <label>
-        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-    </label>
-
-    <button type="submit">
-        Login
-    </button>
-    <a class="button button-outline" href="{{ route('register') }}">Register</a>
+    <div class="text-center">
+        <p></p>
+        <label>
+            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+        </label>
+        <p></p>
+        <button type="submit">
+            Login
+        </button>
+        <p></p>
+        <a class="button button-outline" href="{{ route('register') }}">Register</a>
+    </div>
 </form>
+<p></p>
 @endsection
