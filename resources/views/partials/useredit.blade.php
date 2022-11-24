db253377499a03b00a8c106adfc16fa1612ff08b
up201906031 <up201906031@fc.up.pt>
2022-11-24 21:27:35 +0000

    Updated user profile edit form

--- resources/views/partials/useredit.blade.php
+++ resources/views/partials/useredit.blade.php
@@ -1,24 +1,33 @@
 <form method="post" action="{{ route('editUser', $user->id) }}">
     {{ csrf_field() }}
     {{ method_field('patch') }}
-
-    <label>Name</label> <br>
-    <input type="text" name="name"  value="{{ $user->name }}" />
-    <p></p>
-    <label>Username</label> <br>
-    <input type="text" name="username"  value="{{ $user->username }}" />
-    <p></p>
-    <label>E-mail</label> <br>
-    <input type="email" name="email"  value="{{ $user->email }}" />
-    <p></p>
-    <label>Password</label> <br>
-    <input type="password" name="password" />
-    <p></p>
-    <label>Confirm Password</label> <br>
-    <input type="password" name="password_confirmation" />
-    <p></p>
-    <input type="submit" value="Submit">
-    <p></p>
+    <br>
+    <div class="text-center">
+        <h3>Edit your profile</h3>
+        <br>
+        <div class="input-group mb-3">
+            <span class="input-group-text">Name</span>
+            <input type="text" class="form-control" placeholder="Name" aria-label="Name" aria-describedby="basic-addon1" name="name"  value="{{ $user->name }}">
+        </div>
+        <div class="input-group mb-3">
+            <span class="input-group-text">Username</span>
+            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" name="username"  value="{{ $user->username }}">
+        </div>
+        <div class="input-group mb-3">
+            <span class="input-group-text">Email</span>
+            <input type="email" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1" name="email"  value="{{ $user->email }}">
+        </div>
+        <div class="input-group mb-3">
+            <span class="input-group-text">Password</span>
+            <input type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1" name="password">
+            <span class="input-group-text">Confirm Password</span>
+            <input type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1" name="password_confirmation">
+        </div>
+        <br>
+        <input type="submit" value="Save Changes">
+        <p><a href="{{route('users.profile', $user->id)}}">Cancel</a></p>
+        <br>
+    </div>
 
     @if ($errors->any())
     <div class="alert alert-danger">
