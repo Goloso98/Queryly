3c134c88839493ae1f41ceb5f2c3e6b2efd6bebd
up201906031 <up201906031@fc.up.pt>
2022-11-24 18:03:20 +0000

    Updated question page to be more aesthetic, fixed answers layout

--- resources/views/pages/questionpage.blade.php
+++ resources/views/pages/questionpage.blade.php
@@ -6,23 +6,26 @@
 @section('title', $question->title)
 
 @section('content')
+    <p></p>
     <article class="post" data-id="{{ $question->id }}">
-    <header>
-        <h2>Title: {{ $question->title }} </h2>
-        @php
-            $role = app\Http\Controllers\UserController::showRole();
-        @endphp
-        @if ($role == 'Administrator' && Auth::id() != $question->userid)
-            <a class="delete" href="#"> Delete Question </a>
-        @endif
-        @if (Auth::id() == $question->userid)
-            <a class="delete" href="#"> Delete Question </a>
-            <a class="btn" aria-current="page" href="{{  route('posts.edit', $question->id)  }}">Edit</a>
-            @endif
-    </header>
-    <p> {{ $question->posttext }} </p>
-    <p> {{ $question->postdate }} </p>
-    <p> <a class="btn" aria-current="page" href="{{route('users.profile', $question->userid)}}">&#64;{{ $question->user()->first()->username }}</a></p>
+        <div class="card">
+            <div class="card-body">
+                <h2 class="card-title">Title: {{ $question->title }}</h2>
+                @php
+                    $role = app\Http\Controllers\UserController::showRole();
+                @endphp
+                @if ($role == 'Administrator' && Auth::id() != $question->userid)
+                    <a class="delete" href="#"> Delete Question </a>
+                @endif
+                @if (Auth::id() == $question->userid)
+                    <a class="delete" href="#"> Delete My Question </a>
+                    <a class="btn" aria-current="page" href="{{  route('posts.edit', $question->id)  }}">Edit</a>
+                @endif
+                <p class="card-text">{{ $question->posttext }}</p>
+                {{ $question->postdate }}
+                <a class="btn" aria-current="page" href="{{route('users.profile', $question->userid)}}">&#64;{{ $question->user()->first()->username }}</a>
+            </div>
+        </div>
     </article>
     <p></p>
     <header>
@@ -32,10 +35,11 @@
         <h3>Answers: ({{count($answers)}})</h3>
         <a class="btn" aria-current="page" href="{{ route('posts.addAnswer') }}?question={{$question->id}}"> Post Answer </a>
     </header>
+    <p></p>
     <ul>
         @foreach($answers as $answer)
             @include('partials.answer', ['showTitle' => FALSE])
         @endforeach
     </ul>
-    
+
 @endsection
--- resources/views/partials/answer.blade.php
+++ resources/views/partials/answer.blade.php
@@ -21,3 +21,4 @@
         </div>
     </div>
 </article>
+<p></p>
