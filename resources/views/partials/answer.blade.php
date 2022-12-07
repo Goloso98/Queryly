9477e3f58a28359bb839f82000d43fd4f803e1b9
up201906031 <up201906031@fc.up.pt>
2022-12-07 09:58:53 +0000

    Fixed accordion for comments on answers

--- resources/views/partials/answer.blade.php
+++ resources/views/partials/answer.blade.php
@@ -31,11 +31,11 @@
             <div class="accordion" id="accordionExample">
                 <div class="accordion-item">
                     <h2 class="accordion-header" id="headingTwo">
-                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                             Show Comments ({{count($answerComments)}})
                         </button>
                     </h2>
-                    <div id="collapseTwo" class="accordion-collapse collapse show" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                         <div class="accordion-body">
                             @foreach($answerComments as $comment)
                                 @include('partials.comment')
