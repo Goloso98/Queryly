0b10c50ae123d61e08effe3af2c7fc77a1d6551f
up201906031 <up201906031@fc.up.pt>
2022-12-25 16:20:15 +0000

    Fixed viewing other people's questions or answers pages

--- resources/views/pages/useranswers.blade.php
+++ resources/views/pages/useranswers.blade.php
@@ -4,13 +4,21 @@
 
 @section('content')
 <br>
-<h2 class="text-center">Your Answers</h2>
+@if(Auth::id() == $user->id)
+  <h2 class="text-center">Your Answers</h2>
+@else
+  <h2 class="text-center">{{ $user->username }}'s Answers</h2>
+@endif
 <br>
 <ul>
   @forelse($answers as $answer)
     @include('partials.answer', ['showTitle' => TRUE])
   @empty
-    <p class="info-message">You haven't posted any answers yet.</p>
+    @if(Auth::id() == $user->id)
+      <p class="info-message">You haven't posted any answers yet.</p>
+    @else
+      <p class="info-message">{{ $user->username }} hasn't posted any answers yet.</p>
+    @endif
   @endforelse
 </ul>
 
--- resources/views/pages/userquestions.blade.php
+++ resources/views/pages/userquestions.blade.php
@@ -4,13 +4,21 @@
 
 @section('content')
 <br>
-<h2 class="text-center">Your Questions</h2>
+@if(Auth::id() == $user->id)
+  <h2 class="text-center">Your Questions</h2>
+@else
+  <h2 class="text-center">{{ $user->username }}'s Questions</h2>
+@endif
 <br>
 <ul>
   @forelse($questions as $question)
     @include('partials.question', ['showUser' => FALSE])
   @empty
-    <p class="info-message">You haven't posted any questions yet.</p>
+    @if(Auth::id() == $user->id)
+      <p class="info-message">You haven't posted any questions yet.</p>
+    @else
+      <p class="info-message">{{ $user->username }} hasn't posted any questions yet.</p>
+    @endif
   @endforelse
 </ul>
 
