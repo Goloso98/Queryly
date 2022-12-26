61089a179e93faaa7cbbff79d97463c388f6e4e8
up201906031 <up201906031@fc.up.pt>
2022-12-26 16:49:32 +0000

    Fixed showing questions and answers to search results

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -1,11 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
+    @php
+        $length = $posts->count();;
+    @endphp
+    <br>
+    <h2 class="text-center">Search Results ({{ $length }})</h2>
+    <br>
     @include('partials.emsearchbar')
     
     <ul>
-        @forelse($questions as $question)
-            @include('partials.question', ['showUser' => TRUE])
+        @forelse($posts as $post)
+            @if($post->posttype == 'question')
+                @php
+                    $question = $post;
+                @endphp
+                @include('partials.question', ['showUser' => TRUE])
+            @else
+                @php
+                    $answer = $post;
+                @endphp
+                @include('partials.answer', ['showTitle' => TRUE])
+            @endif
         @empty
             <p>No results found</p>
         @endforelse
