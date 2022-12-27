7dd41f8544713d59b3208d71e5e156a919598c96
up201908209 <up201908209@fc.up.pt>
2022-12-27 18:14:22 +0000

    error found and fixed

--- app/Http/Controllers/StarController.php
+++ app/Http/Controllers/StarController.php
@@ -14,17 +14,14 @@ use App\Models\Star;
 
 class StarController extends Controller
 {
-    public function create($postid){
-        $userid = Auth::id();
-
+    public function create($userid, $postid){
         if (Auth::check()){
             DB::table('stars')->insert(['postid' => $postid, 'userid' => $userid]);
         }
         return;
     }
 
-    public function delete($postid){
-        $userid = Auth::id();
+    public function delete($userid, $postid){
         $star = Star::where('userid', $userid)->where('postid', $postid)->get();
         $stardelete = Star::find($star[0]->id);
         $this->authorize('unlikeaction', $stardelete);
