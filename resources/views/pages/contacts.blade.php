e7b4ae276f7fcb9bfd10510ac93a2267215a3c83
Guilherme Bica <up201705374@fe.up.pt>
2022-12-30 02:01:03 +0000

    Added Conctats Page + form

--- resources/views/pages/contacts.blade.php
+++ resources/views/pages/contacts.blade.php
@@ -1,5 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    <p>Nothing to see yet!</p>
+  <head>
+    <title>Contact Us</title>
+  </head>
+  <body>
+    <h1>Contact Us</h1>
+    <p>Thank you for considering reaching out to us. We are here to help and answer any questions you may have. Please don't hesitate to contact us.</p>
+    <h2>Contact Information</h2>
+    <ul>
+      <li>Phone: 9199pfñmeencomode</li>
+      <li>Email: albertojoaquim420@gmail.com</li>
+      <li>Address: Rua das Flores 69</li>
+    </ul>
+    <h2>Send Us a Message</h2>
+    <form method="POST">
+      @csrf 
+      <label for="name">Name:</label><br>
+      <input type="text" id="name" name="name"><br>
+      <label for="email">Email:</label><br>
+      <input type="email" id="email" name="email"><br>
+      <label for="message">Message:</label><br>
+      <textarea id="message" name="message"></textarea><br>
+      <input type="submit" value="Submit">
+    </form> 
+  </body>
 @endsection
