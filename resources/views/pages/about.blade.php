71adbcd7c7d665f06dd090cab9443846b4f7d1b1
Guilherme Bica <up201705374@fe.up.pt>
2022-12-30 00:26:11 +0000

    Added About Us page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-    <p>Nothing to see yet!</p>
+  <head>
+    <title>About Us</title>
+  </head>
+  <body>
+    <h1>About Us</h1>
+    <p>Welcome! Here you can learn more about us and what we do.</p>
+    <h2>Our Mission</h2>
+    <p>For everyone from all generations that needs help to find the solution to their problem, Queryly is a web-based system that allows you to post all your questions that need an answer, and/or post answers to help other users find the way to the solution of their problem. Unlike other services, our product is a safe space for all users no matter race, gender, sexuality or age. Queryly is designed to be a helpful system for anyone that decides to join our community.</p>
+    <h2>Our History</h2>
+    <p>Founded in 2022 under LBAW course unit. Since then, we have created our product to better serve our customers.</p>
+    <h2>Our Team</h2>
+    <p>Our team is made up of dedicated and talented individuals who are committed to delivering the best experience for our customers. Meet our team members below:</p>
+    <ul>
+      <li>
+        <h3>Ana Sofia Teixeira</h3>
+        <p>uwu</p>
+      </li>
+      <li>
+        <h3>Gabriel Alves</h3>
+        <p>uwu</p>
+      </li>
+      <li>
+        <h3>Guilherme Bica</h3>
+        <p>uwu</p>
+      </li>
+      <li>
+        <h3>Margarida Nazaré</h3>
+        <p>uwu</p>
+      </li>
+    </ul>
+  </body>
 @endsection
