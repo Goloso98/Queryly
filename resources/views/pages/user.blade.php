540c71c9b7edf788b5783771d5955daa4ac8a0fa
up201906031 <up201906031@fc.up.pt>
2022-12-31 22:01:52 +0000

    Wrapped user page into a card and cleaned unused css code

--- resources/views/pages/user.blade.php
+++ resources/views/pages/user.blade.php
@@ -37,89 +37,88 @@
       @endforeach
     </div> <!-- end .flash-message -->
 
-    <div class="row">
-      <div class="col-8">
-        <header>
-          <h2>{{ $user->name }}</h2>
-          @if($roleAdmin || $roleMod)
-            <p class="role">{{ $roleText }}</p>
-          @endif
-        </header>
-        <p>&#64;{{ $user->username }}</p>
-        <p>Email: {{ $user->email }}</p>
-        <p>Age: {{ $carbon::parse($user->birthday)->diff($carbon::now())->y }}</p>
-        @if($user->isblocked)
-          <span class="report small">This account is blocked.</span>
-        @endif
-      </div>
-
-      <div class="col-12 col-sm-4">
-        <img src="{{ $user->avatar }}" class="rounded img-fluid" alt="Profile Pictture">
-      </div>
-
-    </div>
-
-    <hr>
-
-    @if(Auth::check() && Auth::user() == $user)
-      <div class="container centering">
-        <div class="row">
-          <div class="col">
-            <p><a class="btn" aria-current="page" href="{{ route('users.questions', $user->id) }}"> My questions </a></p>
-            <p><a class="btn" aria-current="page" href="{{ route('users.answers', $user->id) }}"> My answers </a></p>
-          </div>
-          <div class="col">
-            <p><a class="btn" aria-current="page" href="{{ route('users.tags', $user->id) }}"> See Followed Tags </a></p>
-            <p><a class="btn" aria-current="page" href="{{ route('users.badges', $user->id) }}"> My Badges </a></p>
-          </div>
-        </div>
-        @if($authAdmin)
-          <p><a class="btn" aria-current="page" href="{{ route('admin.messages') }}">Messages</a></p>
-          <p><a class="btn" aria-current="page" href="{{ route('admin.blocked') }}">See Blocked Users</a></p>
-        @endif
-        @if($authMod)
-          <p><a class="btn" aria-current="page" href="{{ route('users.manageReports', $user->id) }}"> Manage Reports </a></p>
-        @endif
-      </div>
-      <hr>
-      <div class="centering">
-        <p><a class="btn" aria-current="page" href="{{ route('editUser', $user->id) }}"> Edit </a></p>
-        <form method="post" action="{{ route('users.delete', $user->id) }}">
-          @csrf
-          <button type="submit" class="btn">  Delete My Account </button>
-        </form>
-      </div>
-    @else
-      <div class="container centering">
-        <div class="row">
-          <div class="col">
-            <p><a class="btn" aria-current="page" href="{{ route('users.questions', $user->id) }}"> See questions </a></p>
-            <p><a class="btn" aria-current="page" href="{{ route('users.answers', $user->id) }}"> See answers </a></p>
-          </div>
-          <div class="col">
-            <p><a class="btn" aria-current="page" href="{{ route('users.tags', $user->id) }}"> See Followed Tags </a></p>
-            <p><a class="btn" aria-current="page" href="{{ route('users.badges', $user->id) }}"> See Badges </a></p>
-          </div>
-        </div>
-      </div>
-      <hr>
-
-      <div class="centering">
-        @if($authAdmin && $user->id != Auth::id())
-          <form method="post" action="{{ route('users.block', $user->id) }}">
-            @csrf
-            @method('PATCH')
-            <p>
-              <button type="submit" class="btn outlined">
-                @if($user->isblocked)
-                  Unblock Account
-                @else
-                  Block Account
+    <div class="card">
+      <div class="card-body">
+        <div class="card-title">
+          <div class="row">
+            <div class="col-8">
+              <header>
+                <h2>{{ $user->name }}</h2>
+                @if($roleAdmin || $roleMod)
+                  <p class="role">{{ $roleText }}</p>
                 @endif
-              </button>
-            </p>
-          </form>
-        @endif
+              </header>
+              <p>&#64;{{ $user->username }}</p>
+              <p>Email: {{ $user->email }}</p>
+              <p>Age: {{ $carbon::parse($user->birthday)->diff($carbon::now())->y }}</p>
+              @if($user->isblocked)
+                <span class="report small">This account is blocked.</span>
+              @endif
+            </div>
+            <div class="col-12 col-sm-4">
+              <img src="{{ $user->avatar }}" class="rounded img-fluid" alt="Profile Pictture">
+            </div>
+          </div>
+          <hr>
+        </div>
+        <div class="card-text">
+          @if(Auth::check() && Auth::user() == $user)
+            <div class="container centering">
+              <div class="row">
+                <div class="col">
+                  <p><a class="btn" aria-current="page" href="{{ route('users.questions', $user->id) }}"> My questions </a></p>
+                  <p><a class="btn" aria-current="page" href="{{ route('users.answers', $user->id) }}"> My answers </a></p>
+                </div>
+                <div class="col">
+                  <p><a class="btn" aria-current="page" href="{{ route('users.tags', $user->id) }}"> See Followed Tags </a></p>
+                  <p><a class="btn" aria-current="page" href="{{ route('users.badges', $user->id) }}"> My Badges </a></p>
+                </div>
+              </div>
+              @if($authAdmin)
+                <p><a class="btn" aria-current="page" href="{{ route('admin.messages') }}">Messages</a></p>
+                <p><a class="btn" aria-current="page" href="{{ route('admin.blocked') }}">See Blocked Users</a></p>
+              @endif
+              @if($authMod)
+                <p><a class="btn" aria-current="page" href="{{ route('users.manageReports', $user->id) }}"> Manage Reports </a></p>
+              @endif
+            </div>
+          </div>          
+          @else
+            <div class="container centering">
+              <div class="row">
+                <div class="col">
+                  <p><a class="btn" aria-current="page" href="{{ route('users.questions', $user->id) }}"> See questions </a></p>
+                  <p><a class="btn" aria-current="page" href="{{ route('users.answers', $user->id) }}"> See answers </a></p>
+                </div>
+                <div class="col">
+                  <p><a class="btn" aria-current="page" href="{{ route('users.tags', $user->id) }}"> See Followed Tags </a></p>
+                  <p><a class="btn" aria-current="page" href="{{ route('users.badges', $user->id) }}"> See Badges </a></p>
+                </div>
+              </div>
+            </div>
+          @endif
+        </div>
+      </div>
+    </div>
+    
+    <div class="centering">
+      <br>
+      @if($authAdmin && $user->id != Auth::id())
+      <form method="post" action="{{ route('users.block', $user->id) }}">
+        @csrf
+        @method('PATCH')
+        <p>
+          <button type="submit" class="btn outlined">
+            @if($user->isblocked)
+              Unblock Account
+            @else
+              Block Account
+            @endif
+          </button>
+        </p>
+      </form>
+      @endif
+      @if($roleAdmin && $user->id != Auth::id())
         @can('update', $user) 
           <p><a class="btn cardBtn" aria-current="page" href="{{ route('editUser', $user->id) }}"> Edit </a></p>
         @endcan
@@ -129,8 +128,18 @@
             <button type="submit" class="btn cardBtn">  Delete Account </button>
           </form>
         @endcan
-      </div>
-    @endif
+      @else
+        @can('update', $user) 
+          <p><a class="btn" aria-current="page" href="{{ route('editUser', $user->id) }}"> Edit </a></p>
+        @endcan
+        @can('delete', $user)
+          <form method="post" action="{{ route('users.delete', $user->id) }}">
+            @csrf
+            <button type="submit" class="btn">  Delete My Account </button>
+          </form>
+        @endcan
+      @endif
+      <br>
+    </div>
   </article>
-<br>
 @endsection
