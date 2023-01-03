90559eaacbb646c802545726a1e1d8c91ec2958c
Gabriel <gooloso98@gmail.com>
2023-01-03 00:52:58 +0000

    script for docker images

--- upload_image.sh
+++ upload_image.sh
@@ -6,10 +6,10 @@ set -e
 IMAGE_NAME=git.fe.up.pt:5050/lbaw/lbaw2223/lbaw2294 # Replace with your group's image name
 
 # Ensure that dependencies are available
-composer install
-php artisan config:clear
-php artisan clear-compiled
-php artisan optimize
+docker exec laravel composer install
+docker exec laravel php artisan config:clear
+docker exec laravel php artisan clear-compiled
+docker exec laravel php artisan optimize
 
 # docker buildx build --push --platform linux/amd64 -t $IMAGE_NAME .
 docker build -f Dockerfile.prod -t $IMAGE_NAME .
